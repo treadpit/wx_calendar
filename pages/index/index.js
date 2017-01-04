0773788f3fbecb9ae26dc3a11e54eecc6043bdff
付登荣 <dengrongf@jumei.com>
2017-01-04 17:00:52 +0800

    initialize project

---
+++ pages/index/index.js
@@ -0,0 +1,112 @@
+const conf = {
+  data: {
+    hasEmptyGrid: false
+  },
+  getSystemInfo() {
+    try {
+      const res = wx.getSystemInfoSync();
+      this.setData({
+        scrollViewHeight: res.windowHeight * res.pixelRatio || 667
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  },
+  getThisMonthDays(year, month) {
+    return new Date(year, month, 0).getDate();
+  },
+  getFirstDayOfWeek(year, month) {
+    return new Date(Date.UTC(year, month - 1, 1)).getDay();
+  },
+  calculateEmptyGrids(year, month) {
+    const firstDayOfWeek = this.getFirstDayOfWeek(year, month);
+    let empytGrids = [];
+    if (firstDayOfWeek > 0) {
+      for (let i = 0; i < firstDayOfWeek; i++) {
+        empytGrids.push(i);
+      }
+      this.setData({
+        hasEmptyGrid: true,
+        empytGrids
+      });
+    } else {
+      this.setData({
+        hasEmptyGrid: false,
+        empytGrids: []
+      });
+    }
+  },
+  calculateDays(year, month) {
+    let days = [];
+
+    const thisMonthDays = this.getThisMonthDays(year, month);
+
+    for (let i = 1; i <= thisMonthDays; i++) {
+      days.push(i);
+    }
+
+    this.setData({
+      days
+    });
+  },
+  onLoad(options) {
+    const date = new Date();
+    const cur_year = date.getFullYear();
+    const cur_month = date.getMonth() + 1;
+    const weeks_ch = ['日', '一', '二', '三', '四', '五', '六'];
+    this.calculateEmptyGrids(cur_year, cur_month);
+    this.calculateDays(cur_year, cur_month);
+    this.getSystemInfo();
+    this.setData({
+      cur_year,
+      cur_month,
+      weeks_ch
+    })
+  },
+  handleCalendar(e) {
+    const handle = e.currentTarget.dataset.handle;
+    const cur_year = this.data.cur_year;
+    const cur_month = this.data.cur_month;
+    if (handle === 'prev') {
+      let newMonth = cur_month - 1;
+      let newYear = cur_year;
+      if (newMonth < 1) {
+        newYear = cur_year - 1;
+        newMonth = 12;
+      }
+
+      this.calculateDays(newYear, newMonth);
+      this.calculateEmptyGrids(newYear, newMonth);
+
+      this.setData({
+        cur_year: newYear,
+        cur_month: newMonth
+      })
+
+    } else {
+      let newMonth = cur_month + 1;
+      let newYear = cur_year;
+      if (newMonth > 12) {
+        newYear = cur_year + 1;
+        newMonth = 1;
+      }
+
+      this.calculateDays(newYear, newMonth);
+      this.calculateEmptyGrids(newYear, newMonth);
+
+      this.setData({
+        cur_year: newYear,
+        cur_month: newMonth
+      })
+    }
+  },
+  onShareAppMessage() {
+    return {
+      title: '小程序日历',
+      desc: '还是新鲜的日历哟',
+      path: 'pages/index/index'
+    }
+  }
+};
+
+Page(conf);
