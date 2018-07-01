a030a6cb75fbad2f923bc5662b640a4c7bc82747
付登荣 <dengrongf@jumei.com>
2018-07-01 11:58:37 +0800

    周视图支持左右滑动切换周

--- src/template/calendar/index.js
+++ src/template/calendar/index.js
@@ -355,14 +355,11 @@ const conf = {
     const { month: sMonth, year: sYear } = selectedDays[ 0 ];
     const { month: dMonth, year: dYear } = days[ 0 ];
     const { calendar = {} } = this.data;
-    if (sMonth === dMonth && sYear === dYear) {
-      if (this.weekMode) {
-        days.map((item, idx) => {
-          if (item.day === selectedDays[ 0 ].day) days[ idx ].choosed = false;
-        });
-      } else {
-        days[ selectedDays[ 0 ].day - 1 ].choosed = false;
-      }
+    if ((sMonth === dMonth && sYear === dYear) && !this.weekMode) days[ selectedDays[ 0 ].day - 1 ].choosed = false;
+    if (this.weekMode) {
+      days.map((item, idx) => {
+        if (item.day === selectedDays[ 0 ].day) days[ idx ].choosed = false;
+      });
     }
     if (calendar.todoLabels) {
       // 过滤所有待办日期中当月有待办事项的日期
@@ -374,15 +371,15 @@ const conf = {
       // hasTodo 是否有待办事项
       if (this.weekMode) {
         days.map((_item, idx) => {
-          if (_item.day === item.day) {
+          if (+_item.day === +item.day) {
             days[ idx ].hasTodo = true;
-            if (selectedDays[ 0 ].day === +item.day) days[ idx ].showTodoLabel = true;
+            if (+selectedDays[ 0 ].day === +item.day) days[ idx ].showTodoLabel = true;
           }
         });
       } else {
         days[ item.day - 1 ].hasTodo = true;
         // showTodoLabel 是否显示待办标记
-        if (selectedDays[ 0 ].day === +item.day) days[ selectedDays[ 0 ].day - 1 ].showTodoLabel = true;
+        if (+selectedDays[ 0 ].day === +item.day) days[ selectedDays[ 0 ].day - 1 ].showTodoLabel = true;
       }
     });
     if (days[ idx ].showTodoLabel) days[ idx ].showTodoLabel = false;
@@ -505,54 +502,76 @@ const conf = {
   },
   calendarTouchmove(e) {
     if (isLeftSlide.call(this, e)) {
+      if (this.weekMode) return conf.calculateNextWeekDays.call(this);
       conf.chooseNextMonth.call(this);
     }
     if (isRightSlide.call(this, e)) {
+      if (this.weekMode) return conf.calculatePrevWeekDays.call(this);
       conf.choosePrevMonth.call(this);
     }
   },
   /**
-   * 计算当前选中日期所在周，并重新渲染日历
-   * @param {object} currentDay 当前选择日期
+   * 更新当前年月
    */
-  selectedDayWeekAllDays(currentDay) {
-    const { days, curYear, curMonth } = this.data.calendar;
-    let { year, month, day } = currentDay;
-    // 判断选中日期的月份是否与当前月份一致
-    if (curYear !== year || curMonth !== month) day = 1;
-    if (curYear !== year) year = curYear;
-    if (curMonth !== month) month = curMonth;
-    const firstWeekDays = conf.firstWeek.call(this, year, month);
-    const lastWeekDays = conf.lastWeek.call(this, year, month);
-    if (firstWeekDays.find(item => item.day === day)) {
-      conf.calculatePrevMonthGrids.call(this, year, month);
-      this.setData({
-        'calendar.days': firstWeekDays,
-        'calendar.lastEmptyGrids': [],
-      });
-    } else if (lastWeekDays.find(item => item.day === day)) {
-      conf.calculateNextMonthGrids.call(this, year, month);
-      this.setData({
-        'calendar.days': lastWeekDays,
-        'calendar.empytGrids': [],
-      });
-    } else {
-      const week = conf.getDayOfWeek(year, month, day);
-      const range = [ day - week, day + (6 - week) ];
-      const daysCut = days.slice(range[ 0 ] - 1, range[ 1 ]);
-      this.setData({
-        'calendar.days': daysCut,
-        'calendar.lastEmptyGrids': [],
-        'calendar.empytGrids': [],
-      });
+  updateCurrYearAndMonth(type) {
+    let { days, curYear, curMonth } = this.data.calendar;
+    const { month: firstMonth, year: firstYear } = days[ 0 ];
+    const { month: lastMonth, year: lastYear } = days[ days.length - 1 ];
+    let Uyear = curYear;
+    let Umonth = curMonth;
+    if (firstMonth !== lastMonth) {
+      if (type === 'prev') {
+        curYear = firstYear;
+        Umonth = firstMonth;
+      } else {
+        curYear = lastYear;
+        Umonth = lastMonth;
+      }
     }
+    const lastDayOfThisMonth = conf.getThisMonthDays(curYear, curMonth);
+    const lastDayOfThisWeek = days[ days.length - 1 ];
+    const firstDayOfThisWeek = days[ 0 ];
+    if ((lastDayOfThisMonth === +lastDayOfThisWeek.day || lastDayOfThisWeek.day + 7 > lastDayOfThisMonth) && type === 'next') {
+      Umonth = Umonth + 1;
+      if (Umonth > 12) {
+        Uyear = Uyear + 1;
+        Umonth = 12;
+      }
+    } else if (+firstDayOfThisWeek.day <= 7 && type === 'prev') {
+      Umonth = Umonth - 1;
+      if (Umonth <= 0) {
+        Uyear = Uyear - 1;
+        Umonth = 12;
+      }
+    }
+    return {
+      Uyear,
+      Umonth,
+    };
+  },
+  /**
+   * 计算周视图下当前这一周和当月的最后一天
+   */
+  calculateLastDay() {
+    const { days, curYear, curMonth } = this.data.calendar;
+    const lastDayInThisWeek = days[ days.length - 1 ].day;
+    const lastDayInThisMonth = conf.getThisMonthDays(curYear, curMonth);
+    return { lastDayInThisWeek, lastDayInThisMonth };
+  },
+  /**
+   * 计算周视图下当前这一周第一天
+   */
+  calculateFirstDay() {
+    const { days } = this.data.calendar;
+    const firstDayInThisWeek = days[ 0 ].day;
+    return { firstDayInThisWeek };
   },
   /**
    * 当月第一周所有日期范围
    * @param {number} year
    * @param {number} month
    */
-  firstWeek(year, month) {
+  firstWeekInMonth(year, month) {
     const firstDay = conf.getDayOfWeek(year, month, 1);
     const firstWeekDays = [ 1, 1 + (6 - firstDay) ];
     const { days } = this.data.calendar;
@@ -564,13 +583,178 @@ const conf = {
    * @param {number} year
    * @param {number} month
    */
-  lastWeek(year, month) {
+  lastWeekInMonth(year, month) {
     const lastDay = conf.getThisMonthDays(year, month);
     const lastDayWeek = conf.getDayOfWeek(year, month, lastDay);
     const lastWeekDays = [ lastDay - lastDayWeek, lastDay ];
     const { days } = this.data.calendar;
     const daysCut = days.slice(lastWeekDays[ 0 ] - 1, lastWeekDays[ 1 ]);
     return daysCut;
+  },
+  /**
+   * 渲染日期之前初始化已选日期
+   * @param {array} days 当前日期数组
+   */
+  initSelectedDay(days) {
+    const daysCopy = days.slice();
+    const { selectedDay } = this.data.calendar;
+    const selectedDayStr = selectedDay.map(item => `${item.year}+${item.month}+${item.day}`);
+    daysCopy.map(item => {
+      if (selectedDayStr.indexOf(`${item.year}+${item.month}+${item.day}`) !== -1) {
+        item.choosed = true;
+      } else {
+        item.choosed = false;
+      }
+    });
+    return daysCopy;
+  },
+  /**
+   * 计算下一周的日期
+   */
+  calculateNextWeekDays() {
+    let { lastDayInThisWeek, lastDayInThisMonth } = conf.calculateLastDay.call(this);
+    let { curYear, curMonth } = this.data.calendar;
+    let days = [];
+    if (lastDayInThisMonth - lastDayInThisWeek >= 7) {
+      const { Uyear, Umonth } = conf.updateCurrYearAndMonth.call(this, 'next');
+      curYear = Uyear;
+      curMonth = Umonth;
+      for (let i = lastDayInThisWeek + 1; i <= lastDayInThisWeek + 7; i++) {
+        days.push({
+          year: curYear,
+          month: curMonth,
+          day: i,
+        });
+      }
+    } else {
+      for (let i = lastDayInThisWeek + 1; i <= lastDayInThisMonth; i++) {
+        days.push({
+          year: curYear,
+          month: curMonth,
+          day: i,
+        });
+      }
+      const { Uyear, Umonth } = conf.updateCurrYearAndMonth.call(this, 'next');
+      curYear = Uyear;
+      curMonth = Umonth;
+      for (let i = 1; i <= 7 - (lastDayInThisMonth - lastDayInThisWeek); i++) {
+        days.push({
+          year: curYear,
+          month: curMonth,
+          day: i,
+        });
+      }
+    }
+    days = conf.initSelectedDay.call(this, days);
+    this.setData({
+      'calendar.curYear': curYear,
+      'calendar.curMonth': curMonth,
+      'calendar.days': days,
+    });
+  },
+  /**
+   * 计算上一周的日期
+   */
+  calculatePrevWeekDays() {
+    let { firstDayInThisWeek } = conf.calculateFirstDay.call(this);
+    let { curYear, curMonth } = this.data.calendar;
+    let days = [];
+
+    if (firstDayInThisWeek - 7 > 0) {
+      const { Uyear, Umonth } = conf.updateCurrYearAndMonth.call(this, 'prev');
+      curYear = Uyear;
+      curMonth = Umonth;
+      for (let i = firstDayInThisWeek - 7; i < firstDayInThisWeek; i++) {
+        days.push({
+          year: curYear,
+          month: curMonth,
+          day: i,
+        });
+      }
+    } else {
+      let temp = [];
+      for (let i = 1; i < firstDayInThisWeek; i++) {
+        temp.push({
+          year: curYear,
+          month: curMonth,
+          day: i,
+        });
+      }
+      const { Uyear, Umonth } = conf.updateCurrYearAndMonth.call(this, 'prev');
+      curYear = Uyear;
+      curMonth = Umonth;
+      const prevMonthDays = conf.getThisMonthDays(curYear, curMonth);
+      for (let i = prevMonthDays - Math.abs((firstDayInThisWeek - 7)); i <= prevMonthDays; i++) {
+        days.push({
+          year: curYear,
+          month: curMonth,
+          day: i,
+        });
+      }
+      days = days.concat(temp);
+    }
+    days = conf.initSelectedDay.call(this, days);
+    this.setData({
+      'calendar.curYear': curYear,
+      'calendar.curMonth': curMonth,
+      'calendar.days': days,
+    });
+  },
+  /**
+   * 计算当前选中日期所在周，并重新渲染日历
+   * @param {object} currentDay 当前选择日期
+   */
+  selectedDayWeekAllDays(currentDay) {
+    let { days, curYear, curMonth } = this.data.calendar;
+    let { year, month, day } = currentDay;
+    // 判断选中日期的月份是否与当前月份一致
+    if (curYear !== year || curMonth !== month) day = 1;
+    if (curYear !== year) year = curYear;
+    if (curMonth !== month) month = curMonth;
+    const firstWeekDays = conf.firstWeekInMonth.call(this, year, month);
+    let lastWeekDays = conf.lastWeekInMonth.call(this, year, month);
+    let empytGrids = [];
+    let lastEmptyGrids = [];
+    if (firstWeekDays.find(item => item.day === day)) { // 当前选择的日期为该月第一周
+      const lastDayInThisMonth = conf.getThisMonthDays(year, month);
+      let temp = [];
+      const { Uyear, Umonth } = conf.updateCurrYearAndMonth.call(this, 'prev');
+      curYear = Uyear;
+      curMonth = Umonth;
+      for (let i = lastDayInThisMonth - (7 - firstWeekDays.length) + 1; i <= lastDayInThisMonth; i++) {
+        temp.push({
+          year: curYear,
+          month: curMonth,
+          day: i,
+        });
+      }
+      days = temp.concat(firstWeekDays);
+    } else if (lastWeekDays.find(item => item.day === day)) { // 当前选择的日期为该月最后一周
+      const temp = [];
+      if (lastWeekDays && lastWeekDays.length < 7) {
+        const { Uyear, Umonth } = conf.updateCurrYearAndMonth.call(this, 'next');
+        curYear = Uyear;
+        curMonth = Umonth;
+        for (let i = 1, len = 7 - lastWeekDays.length; i <= len; i++) {
+          temp.push({
+            year: curYear,
+            month: curMonth,
+            day: i,
+          });
+        }
+      }
+      days = lastWeekDays.concat(temp);
+    } else {
+      const week = conf.getDayOfWeek(year, month, day);
+      const range = [ day - week, day + (6 - week) ];
+      days = days.slice(range[ 0 ] - 1, range[ 1 ]);
+    }
+    days = conf.initSelectedDay.call(this, days);
+    this.setData({
+      'calendar.days': days,
+      'calendar.empytGrids': empytGrids,
+      'calendar.lastEmptyGrids': lastEmptyGrids,
+    });
   },
   /**
    * 周、月视图切换
