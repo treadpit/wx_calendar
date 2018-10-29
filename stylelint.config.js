6dd882515f55b42a3f88bdd0213cc770405055f8
付登荣 <dengrongf@jumei.com>
2018-10-29 15:36:51 +0800

    优化getCurrentPage重复读取

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   defaultSeverity: 'error',
   extends: 'stylelint-config-wxss',
-  plugins: [
-    'stylelint-no-z-index'
-  ],
+  plugins: ['stylelint-no-z-index'],
   rules: {
     'at-rule-no-vendor-prefix': true,
-    'indentation': 2,
+    indentation: 2,
     'media-feature-name-no-vendor-prefix': true,
     'no-missing-end-of-source-newline': null,
     'plugin/no-z-index': 2,
@@ -14,7 +12,7 @@ module.exports = {
     'selector-max-compound-selectors': 1,
     'selector-no-vendor-prefix': true,
     'value-no-vendor-prefix': true,
-    'selector-type-no-unknown': [true, {'ignoreTypes': [ 'page', 'navigator' ]}],
-    'unit-no-unknown': [true, {'ignoreUnits': [ 'rpx' ]}]
+    'selector-type-no-unknown': [true, { ignoreTypes: ['page', 'navigator'] }],
+    'unit-no-unknown': [true, { ignoreUnits: ['rpx'] }]
   }
 };
