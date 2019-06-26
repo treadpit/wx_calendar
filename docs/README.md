11a3a9d20b2f0292b8a0ee2fa0a83de8a927b7cc
付登荣 <dengrongf@jumei.com>
2019-06-26 16:05:04 +0800

    more calendar in one page #110

--- docs/README.md
+++ docs/README.md
@@ -12,7 +12,7 @@
 
 #### 1. 引入组件
 
-b将 `src/component` 文件夹拷贝至自己的组件目录，页面 `json` 文件中配置组件
+将 `src/component` 文件夹拷贝至自己的组件目录，页面 `json` 文件中配置组件
 
 ```json
 {
@@ -96,6 +96,62 @@ const conf = {
   }
 };
 Page(conf);
+```
+
+#### 4. 多日历组件
+
+同一页面如果有多个不同配置的日历组件，可分别定义配置。
+
+组件必须有ID属性，在调用提供的各个日历API时需注意，最后一个参数为要操作的组件ID（多个组件模式下必传），表示当前要操作的是哪一个日历组件数据。
+
+具体示例可参考  [/pages/calendarMoreComponent/index页面](https://github.com/treadpit/wx_calendar/tree/master/src/pages/calendarMoreComponent)
+
+```js
+import { getTodoLabels } from '../../component/calendar/main.js';
+
+Page({
+  data: {
+    calendarConfig: {
+      multi: true,
+      onlyShowCurrentMonth: 1
+    },
+    calendarConfig2: {
+      inverse: 1, // 单选模式下是否可以取消选择
+      defaultDay: '2019-5-20'
+    }
+  },
+  doSomething() {
+    const todoLabels = getTodoLabels('#calendar1');
+    setTodoLabels({
+      circle: true,
+      days: [{
+        year: 2018,
+        month: 1,
+        day: 1,
+        todoText: '待办'
+      }],
+    }, '#calendar2');
+  }
+})
+```
+
+```xml
+<calendar
+  id="calendar1"
+  calendarConfig="{{calendarConfig}}"
+  bind:afterTapDay="afterTapDay"
+  bind:whenChangeMonth="whenChangeMonth"
+  bind:onTapDay="onTapDay"
+  bind:afterCalendarRender="afterCalendarRender"
+></calendar>
+<calendar
+    id="calendar2"
+    calendarConfig="{{calendarConfig2}}"
+    bind:afterTapDay="afterTapDay"
+    bind:whenChangeMonth="whenChangeMonth"
+    bind:onTapDay="onTapDay"
+    bind:afterCalendarRender="afterCalendarRender"
+  ></calendar>
 ```
 
 #### 日历事件使用说明
---
+++ src/pages/calendarMoreComponent/index.js
@@ -0,0 +1,99 @@
+import {
+  setTodoLabels,
+  // enableArea,
+  disableDay
+} from '../../component/calendar/main.js';
+
+const conf = {
+  data: {
+    calendarConfig: {
+      multi: true,
+      // inverse: 1, // 单选模式下是否可以取消选择
+      // defaultDay: '2019-5-19'
+      onlyShowCurrentMonth: 1
+      // disablePastDay: 1
+    },
+    calendarConfig2: {
+      // multi: true,
+      inverse: 1, // 单选模式下是否可以取消选择
+      defaultDay: '2019-5-20'
+      // disablePastDay: 1
+    }
+  },
+  setTodo() {
+    const data = [
+      {
+        year: '2019',
+        month: '6',
+        day: '15'
+      },
+      {
+        year: 2019,
+        month: 6,
+        day: 18,
+        todoText: '待办'
+      }
+    ];
+
+    setTodoLabels(
+      {
+        // circle: true,
+        // pos: 'top',
+        days: data
+      },
+      '#start'
+    );
+  },
+  afterTapDay1(e) {
+    console.log('afterTapDay', e.detail);
+  },
+  whenChangeMonth1(e) {
+    console.log('whenChangeMonth', e.detail);
+  },
+  onTapDay1(e) {
+    console.log('onTapDay', e.detail);
+  },
+  afterCalendarRender1(e) {
+    console.log('afterCalendarRender', e);
+    this.setTodo();
+    disableDay(
+      [
+        {
+          year: '2019',
+          month: '6',
+          day: '15'
+        },
+        {
+          year: '2019',
+          month: '6',
+          day: '30'
+        }
+      ],
+      '#start'
+    );
+  },
+  afterCalendarRender2(e) {
+    console.log('afterCalendarRender', e);
+    this.setTodo();
+    disableDay(
+      [
+        {
+          year: '2019',
+          month: '5',
+          day: '10'
+        },
+        {
+          year: '2019',
+          month: '5',
+          day: '28'
+        }
+      ],
+      '#end'
+    );
+    setTimeout(() => {
+      disableDay([], '#end');
+    }, 2000);
+  }
+};
+
+Page(conf);
