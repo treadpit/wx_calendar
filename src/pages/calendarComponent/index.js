ebedca26ac9c54927311b147cfc706d6eb62f20c
付登荣 <dengrongf@jumei.com>
2019-07-18 19:40:31 +0800

    #141

--- src/pages/calendarComponent/index.js
+++ src/pages/calendarComponent/index.js
@@ -6,12 +6,6 @@ const conf = {
       // defaultDay: '2019-5-19'
       onlyShowCurrentMonth: 1
       // disablePastDay: 1
-    },
-    calendarConfig2: {
-      // multi: true,
-      // inverse: 1, // 单选模式下是否可以取消选择
-      defaultDay: '2019-5-20'
-      // disablePastDay: 1
     }
   },
   onShow: function() {
@@ -22,12 +16,12 @@ const conf = {
     const data = [
       {
         year: '2019',
-        month: '5',
+        month: '7',
         day: '15'
       },
       {
         year: 2019,
-        month: 5,
+        month: 8,
         day: 18,
         todoText: '待办'
       }
@@ -35,8 +29,8 @@ const conf = {
     // 异步请求
     setTimeout(() => {
       this.calendar.setTodoLabels({
-        // circle: true,
-        pos: 'top',
+        circle: true,
+        // pos: 'top',
         days: data
       });
     }, 1000);
