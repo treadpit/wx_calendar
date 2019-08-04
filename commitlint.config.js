fb8392e25c2db39e82cfba54954378bc9a4c54c0
付登荣 <dengrongf@jumei.com>
2019-08-04 10:17:10 +0800

    chore: update .editorconfig and add commit msg lint

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 70],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'docs', // Adds or alters documentation.
+        'chore', // Other changes that don't modify src or test files.
+        'feat', // Adds a new feature.
+        'fix', // Solves a bug.
+        'merge', // Merge branch ? of ?.
+        'perf', // Improves performance.
+        'refactor', // Rewrites code without feature, performance or bug changes.
+        'revert', // Reverts a previous commit.
+        'style', // Improves formatting, white-space.
+        'test' // Adds or modifies tests.
+      ]
+    ]
+  }
+};
