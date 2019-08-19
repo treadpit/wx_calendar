7f55c92b3fe4b6862c2b0fc77aa7cb2da8240aa1
付登荣 <dengrongf@jumei.com>
2019-08-19 14:39:14 +0800

    chore: build calendar component

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,56 @@
+const del = require('del');
 const gulp = require('gulp');
-const debug = require('gulp-debug');
 const path = require('path');
+const debug = require('gulp-debug');
 
 const rootPath = path.join(__dirname, 'src');
 
-function fileCopy() {
+function copyExample() {
   return gulp
     .src(['src/**/*'], {
       base: rootPath
     })
     .pipe(
       debug({
-        title: '文件复制:'
+        title: '复制:'
       })
     )
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('example'));
 }
 
-gulp.task('default', fileCopy);
+function CopyCompJs() {
+  return gulp
+    .src('dist/*')
+    .pipe(
+      debug({
+        title: '复制:'
+      })
+    )
+    .pipe(gulp.dest('calendar'));
+}
+
+function copyCalendar() {
+  return gulp
+    .src([
+      'src/component/calendar/**/*',
+      '!src/component/calendar/*.js',
+      '!src/component/calendar/{func,func/*}'
+    ])
+    .pipe(
+      debug({
+        title: '复制:'
+      })
+    )
+    .pipe(gulp.dest('calendar'));
+}
+
+exports.copyCalendar = gulp.series(copyCalendar, CopyCompJs);
+
+gulp.task('default', gulp.series(copyCalendar, CopyCompJs));
+gulp.task('example', copyExample);
+gulp.task('clean', function() {
+  return del(['dist']);
+});
 gulp.task('watch', () => {
-  gulp.watch(['src/**/*', 'src/*'], fileCopy);
+  gulp.watch(['src/**/*', 'src/*'], copyExample);
 });
