2ca1d10f6a44a3278bcfa134b210410fb7393c3d
treadpit <todrfu@gmail.com>
2020-11-08 20:48:54 +0800

    feat: update dist of calendar_v1

--- calendar/plugins/preset/index.js
+++
@@ -1,9 +0,0 @@
-import base from './base'
-import getCalendarData from './get-calendar-data'
-
-const preset = [
-  ['base', base()],
-  ['get-calendar-data', getCalendarData()]
-]
-
-export default preset
